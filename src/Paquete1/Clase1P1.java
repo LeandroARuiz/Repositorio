1b295e7ca73c4fc71e16def28a9dad6a15f69c04
LeandroARuiz <leandroruiz480@gmail.com>
2025-02-07 10:57:54 -0300

    Update Clase1P1.java

--- src/Paquete1/Clase1P1.java
+++ src/Paquete1/Clase1P1.java
@@ -3,5 +3,6 @@ package Paquete1;
 public class Clase1P1 {
 public static void main(String[] args) {
     System.out.println("Linea 1. Clase 1, Paquete 1.");
+    System.out.println("Linea 3.")
 }
 }
