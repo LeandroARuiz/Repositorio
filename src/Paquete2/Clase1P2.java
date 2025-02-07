2ddfaf5b37a910ed857340f0f2b53e9d83fa0284
Leandro Ruiz <leandroruiz480@gmail.com>
2025-02-07 11:10:08 -0300

    Ultimo commit.

--- src/Paquete2/Clase1P2.java
+++ src/Paquete2/Clase1P2.java
@@ -4,5 +4,6 @@ public class Clase1P2 {
 public static void main(String[] args) {
     System.out.println("Linea 1. Clase 1, Paquete 2.");
     System.out.println("Linea 2.");
+    System.out.println("Linea 3.");
 }
 }
