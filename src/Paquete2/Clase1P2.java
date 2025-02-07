15a2a7e01fa90dd62b5201070d538a9963232d68
Leandro Ruiz <leandroruiz480@gmail.com>
2025-02-07 10:45:49 -0300

    Segunda version.

--- src/Paquete2/Clase1P2.java
+++ src/Paquete2/Clase1P2.java
@@ -3,5 +3,6 @@ package Paquete2;
 public class Clase1P2 {
 public static void main(String[] args) {
     System.out.println("Linea 1. Clase 1, Paquete 2.");
+    System.out.println("Linea 2.");
 }
 }
