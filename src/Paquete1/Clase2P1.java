b4f9e724d11981e34e26723573afbfcdf7cec30e
Leandro Ruiz <leandroruiz480@gmail.com>
2025-02-15 23:09:35 -0300

    Primer Commit 15/02/2025

---
+++ src/Paquete1/Clase2P1.java
@@ -0,0 +1,11 @@
+package Paquete1;
+
+public class Clase2P1 {
+public static void main(String[] args) {
+    System.out.println("Nueva clase agregada.");
+    int a = 5;
+    int b = 10;
+    int suma = a + b;
+    System.out.println("El resultado final es: " + suma);
+}
+}
