b7d94d2d502058624a26af170e636d9bea5c6c92
Leandro Ruiz <leandroruiz480@gmail.com>
2025-02-15 23:24:21 -0300

    Segundo Commit 15/02/2025

--- src/Paquete1/Clase1P1.java
+++ src/Paquete1/Clase1P1.java
@@ -1,7 +1,12 @@
 package Paquete1;
 
 public class Clase1P1 {
-public static void main(String[] args) {
+
+    public int sumar (int a, int b){
+        return a + b;
+    }
+
+    public static void main(String[] args) {
     System.out.println("Linea 1. Clase 1, Paquete 1.");
     System.out.println("Linea 2.");
     System.out.println("Linea 3.");
--- src/Paquete1/Clase2P1.java
+++ src/Paquete1/Clase2P1.java
@@ -1,11 +1,12 @@
 package Paquete1;
+import Paquete1.Clase1P1;
 
 public class Clase2P1 {
 public static void main(String[] args) {
     System.out.println("Nueva clase agregada.");
     int a = 5;
     int b = 10;
-    int suma = a + b;
+    int suma = sumar(a,b);
     System.out.println("El resultado final es: " + suma);
 }
 }
